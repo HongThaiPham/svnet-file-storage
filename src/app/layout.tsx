a2aca3a6ea6388c56c09ba52e7bb3dddbc62945d
leopham <hongthaipro@gmail.com>
2024-06-24 15:48:49 +0700

    chore: Add @radix-ui/react-slot package to npm dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AppProvider from "@/providers/AppProvider";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
